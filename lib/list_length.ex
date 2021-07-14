ceb32dd23ace44f5dec772ba935aaf39e3b36420
webmasterdro <pedro-octavio@hotmail.com>
2021-07-13 22:55:36 -0300

    Finish challenge

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,9 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: count(list, 0)
+
+  defp count([], acc), do: acc
+
+  defp count([_head | tail], acc) do
+     count(tail, acc + 1)
   end
 end
